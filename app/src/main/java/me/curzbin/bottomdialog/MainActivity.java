364b07eb656407fbfd337adee3f4de8bdc0e1c7c
郑梓斌 <curzbin@sina.com>
2016-07-18 15:39:24 +0800

    添加动画，添加方向，listener监听问题

--- app/src/main/java/me/curzbin/bottomdialog/MainActivity.java
+++ app/src/main/java/me/curzbin/bottomdialog/MainActivity.java
@@ -34,13 +34,52 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.horizontal_single).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 new BottomDialog(MainActivity.this)
                         .title(R.string.share_title)
+                        .orientation(BottomDialog.HORIZONTAL)
                         .inflateMenu(R.menu.menu_share)
                         .itemClick(rxBus)
+                        .itemClick(new OnItemClickListener() {
+                            @Override
+                            public void click(Item item) {
+                                Toast.makeText(MainActivity.this, getString(R.string.share_title) + item.getTitle(), Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        findViewById(R.id.horizontal_multi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new BottomDialog(MainActivity.this)
+                        .title(R.string.share_title)
+                        .orientation(BottomDialog.HORIZONTAL)
+                        .inflateMenu(R.menu.menu_share)
+                        .inflateMenu(R.menu.menu_main)
+                        .itemClick(new OnItemClickListener() {
+                            @Override
+                            public void click(Item item) {
+                                Toast.makeText(MainActivity.this, getString(R.string.share_title) + item.getTitle(), Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        findViewById(R.id.vertical).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new BottomDialog(MainActivity.this)
+                        .title(R.string.title_item)
+                        .orientation(BottomDialog.VERTICAL)
+                        .inflateMenu(R.menu.menu_share)
                         .itemClick(new OnItemClickListener() {
                             @Override
                             public void click(Item item) {
